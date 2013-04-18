23a0b50b799523172ddc5192fd4161e507261eb8
Corey Besmer <coreyonf@cs.utexas.edu>
2013-04-18 17:05:58 -0500

    Modified Abstract Shape and Conway Cell.

--- AbstractCell.h
+++ AbstractCell.h
@@ -1,1 +1,65 @@
-#
+class AbstractCell {
+	friend bool operator == (const AbstractCell& lhs, const AbstractCell& rhs) {
+		return lhs.equals(rhs);
+	}
+
+	friend bool operator != (const AbstractCell& lhs, const AbstractCell& rhs) {
+		return !(lhs == rhs);
+	}
+
+	friend std::istream& operator >> (std::istream& lhs, AbstractCell& rhs) {
+		return rhs.read(lhs);
+	}
+
+	friend std::ostream& operator << (std::ostream& lhs, const AbstractCell& rhs) {
+		return rhs.write(lhs);
+	}
+
+private:
+	bool _alive;
+	Life *_world;
+
+protected:
+	AbstractCell& operator = (const AbstractCell& that) {
+		_alive = that._alive;
+		_world = that._world;
+		return *this;
+	}
+
+	virtual bool equals (const AbstractCell& that) const = 0;
+
+	virtual std::istream& read (std::istream& in) = 0;
+
+	virtual std::ostream& write (std::ostream& out) const = 0;
+
+public:
+	AbstractCell (bool input, Life *environment) :
+			_alive(input),
+			_world(environment)
+		{}
+/*
+	AbstractCell (const AbstractCell& that) :
+			_alive (that._alive),
+			_world (that._world)
+		{}
+*/
+	virtual ~AbstractCell ()
+		{}
+
+	virtual bool mutate () const = 0;
+
+	virtual AbstractCell* clone () const = 0;
+
+};
+
+bool AbstractCell::equals (const AbstractCell& that) const {
+    return (_alive == that._alive) && (_world == that._world);
+}
+
+std::istream& AbstractCell::read (std::istream& in) {
+    return in >> _alive >> _world;
+}
+
+std::ostream& AbstractCell::write (std::ostream& out) const {
+    return out << "Cell alive: " << _alive;
+}
--- ConwayCell.h
+++ ConwayCell.h
@@ -1,1 +1,28 @@
-#
+#include AbstractCell.h
+
+class ConwayCell : public AbstractCell {
+private:
+
+protected:
+        virtual bool equals (const AbstractCell& that) const {
+            if (const ConwayCell* const p = dynamic_cast<const ConwayCell*>(&that))
+                return AbstractCell::equals(*p);
+            return false;}
+
+        virtual std::istream& read (std::istream& in) {
+            return AbstractCell::read(in);}
+
+        virtual std::ostream& write (std::ostream& out) const {
+            return AbstractCell::write(out);}
+public:
+	ConwayCell(bool alive, Life* world) : 
+		AbstractCell(alive, world)
+		{}
+	
+	virtual bool mutate() {
+		//check for neighbors, act accordingly
+	}
+	virtual ConwayCell* clone () const {
+		return new ConwayCell(*this);
+	}
+};
